328769b69f2c98ae14344a0d6c90750a7b2a97ba
Fallbork <73458244+Fallbork@users.noreply.github.com>
2022-10-23 00:43:51 -0300

    Added single system query

--- src/systems.cs
+++ src/systems.cs
@@ -3,13 +3,46 @@
 // Copyright (c) 2012-2022 Leopotam <leopotam@yandex.ru>
 // ----------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
+using System.Text;
 
 #if ENABLE_IL2CPP
 using Unity.IL2CPP.CompilerServices;
 #endif
 
-namespace Leopotam.EcsLite {
+#nullable enable
+
+namespace Leopotam.EcsLite
+{
+    internal static class EcsTypeExtensions
+    {
+        // Code originally written by Brian Rogers in StackOverflow
+        // Source: https://stackoverflow.com/questions/17480990/
+        internal static string TypeName (this Type type)
+        {
+            if (!type.IsGenericType)
+            {
+                return type.Name;
+            }
+
+            StringBuilder sb = new StringBuilder ();
+            sb.Append (type.Name).Append ('<');
+            bool appendComma = false;
+            foreach (Type t in type.GetGenericArguments ()) {
+                sb.Append (appendComma ? "," : "").Append (TypeName(t));
+                appendComma = true;
+            }
+            sb.Append ('>');
+            return sb.ToString ();
+        }
+    }
+
+    internal class KeyedSystem {
+        internal string Key { get; init; }
+        internal IEcsSystem System { get; init; }
+    }
+
     public interface IEcsSystem { }
 
     public interface IEcsPreInitSystem : IEcsSystem {
@@ -37,8 +70,9 @@ namespace Leopotam.EcsLite {
         IEcsSystems AddWorld (EcsWorld world, string name);
         EcsWorld GetWorld (string name = null);
         Dictionary<string, EcsWorld> GetAllNamedWorlds ();
-        IEcsSystems Add (IEcsSystem system);
+        IEcsSystems Add<T> (T system) where T : IEcsSystem;
         List<IEcsSystem> GetAllSystems ();
+        T? GetSystem<T> () where T : IEcsSystem;
         void Init ();
         void Run ();
         void Destroy ();
@@ -51,8 +85,8 @@ namespace Leopotam.EcsLite {
     public class EcsSystems : IEcsSystems {
         readonly EcsWorld _defaultWorld;
         readonly Dictionary<string, EcsWorld> _worlds;
-        readonly List<IEcsSystem> _allSystems;
-        readonly List<IEcsRunSystem> _runSystems;
+        readonly List<KeyedSystem> _allSystems;
+        readonly List<KeyedSystem> _runSystems;
         readonly object _shared;
 #if DEBUG
         bool _inited;
@@ -62,8 +96,8 @@ namespace Leopotam.EcsLite {
             _defaultWorld = defaultWorld;
             _shared = shared;
             _worlds = new Dictionary<string, EcsWorld> (8);
-            _allSystems = new List<IEcsSystem> (128);
-            _runSystems = new List<IEcsRunSystem> (128);
+            _allSystems = new List<KeyedSystem> (128);
+            _runSystems = new List<KeyedSystem> (128);
         }
 
         public virtual T GetShared<T> () where T : class {
@@ -93,27 +127,47 @@ namespace Leopotam.EcsLite {
             return _worlds;
         }
 
-        public virtual IEcsSystems Add (IEcsSystem system) {
+        public virtual IEcsSystems Add<T> (T system) where T : IEcsSystem {
+            string key = typeof (T).TypeName ();
+
 #if DEBUG && !LEOECSLITE_NO_SANITIZE_CHECKS
             if (_inited) { throw new System.Exception ("Cant add system after initialization."); }
+            if (_allSystems.Exists (value => value.Key == key)) { throw new System.Exception ("Cant add systems with identical keys."); }
 #endif
-            _allSystems.Add (system);
+            _allSystems.Add (new KeyedSystem () { Key = key, System = system });
             if (system is IEcsRunSystem runSystem) {
-                _runSystems.Add (runSystem);
+                // No need to check for keys here, both system lists are manipulated concurrently.
+                _runSystems.Add (new KeyedSystem() { Key = key, System = runSystem });
             }
             return this;
         }
 
-        public virtual List<IEcsSystem> GetAllSystems () {
-            return _allSystems;
+        public virtual List<IEcsSystem> GetAllSystems() {
+            List<IEcsSystem> systems = new List<IEcsSystem>(_allSystems.ConvertAll(value => value.System));
+            return systems;
+        }
+
+        // List.Find () returns null if the value is not found. The existence of the key is checked in debug
+        // builds, but it is not guaranteed in release builds. Instead of throwing an exception, we'll notify
+        // the user of the possibility of a not-valid system to be returned, and allow for flexibility of the
+        // response on their side.
+        //
+        // Usage: var system = systems.GetSystem<ExampleSystem>();
+        public virtual T? GetSystem<T> () where T : IEcsSystem {
+            string key = typeof (T).TypeName ();
+            KeyedSystem? system = _allSystems.Find (value => value.Key == key);
+#if DEBUG && !LEOECSLITE_NO_SANITIZE_CHECKS
+            if (system == null) { throw new System.Exception ("System does not exist in the current context."); }
+#endif
+            return (T?)system.System;
         }
 
         public virtual void Init () {
 #if DEBUG && !LEOECSLITE_NO_SANITIZE_CHECKS
             if (_inited) { throw new System.Exception ("Already initialized."); }
 #endif
-            foreach (var system in _allSystems) {
-                if (system is IEcsPreInitSystem initSystem) {
+            foreach (var sysWrapper in _allSystems) {
+                if (sysWrapper.System is IEcsPreInitSystem initSystem) {
                     initSystem.PreInit (this);
 #if DEBUG && !LEOECSLITE_NO_SANITIZE_CHECKS
                     var worldName = CheckForLeakedEntities (this);
@@ -121,8 +175,8 @@ namespace Leopotam.EcsLite {
 #endif
                 }
             }
-            foreach (var system in _allSystems) {
-                if (system is IEcsInitSystem initSystem) {
+            foreach (var sysWrapper in _allSystems) {
+                if (sysWrapper.System is IEcsInitSystem initSystem) {
                     initSystem.Init (this);
 #if DEBUG && !LEOECSLITE_NO_SANITIZE_CHECKS
                     var worldName = CheckForLeakedEntities (this);
@@ -140,7 +194,7 @@ namespace Leopotam.EcsLite {
             if (!_inited) { throw new System.Exception ("Cant run without initialization."); }
 #endif
             for (int i = 0, iMax = _runSystems.Count; i < iMax; i++) {
-                _runSystems[i].Run (this);
+                ((IEcsRunSystem)(_runSystems[i].System)).Run (this);
 #if DEBUG && !LEOECSLITE_NO_SANITIZE_CHECKS
                 var worldName = CheckForLeakedEntities (this);
                 if (worldName != null) { throw new System.Exception ($"Empty entity detected in world \"{worldName}\" after {_runSystems[i].GetType ().Name}.Run()."); }
@@ -150,7 +204,7 @@ namespace Leopotam.EcsLite {
 
         public virtual void Destroy () {
             for (var i = _allSystems.Count - 1; i >= 0; i--) {
-                if (_allSystems[i] is IEcsDestroySystem destroySystem) {
+                if (_allSystems[i].System is IEcsDestroySystem destroySystem) {
                     destroySystem.Destroy (this);
 #if DEBUG && !LEOECSLITE_NO_SANITIZE_CHECKS
                     var worldName = CheckForLeakedEntities (this);
@@ -159,7 +213,7 @@ namespace Leopotam.EcsLite {
                 }
             }
             for (var i = _allSystems.Count - 1; i >= 0; i--) {
-                if (_allSystems[i] is IEcsPostDestroySystem postDestroySystem) {
+                if (_allSystems[i].System is IEcsPostDestroySystem postDestroySystem) {
                     postDestroySystem.PostDestroy (this);
 #if DEBUG && !LEOECSLITE_NO_SANITIZE_CHECKS
                     var worldName = CheckForLeakedEntities (this);
