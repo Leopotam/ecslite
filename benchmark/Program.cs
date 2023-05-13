ecd630aa87ab53803f6ab49824f8ffdbe2c15050
Artem Votintsev <artem.votincev@gmail.com>
2023-05-13 09:39:20 +0200

    Add dotnet benchmark

---
+++ benchmark/Program.cs
@@ -0,0 +1,88 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Leopotam.EcsLite;
+
+[MemoryDiagnoser]
+public class Program
+{
+    public static void Main()
+    {
+        BenchmarkRunner.Run<Program>();
+    }
+
+    private static int iterations = 1000;
+    private static int entities = 1000;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark]
+    public void MultiEntitySwitcherLeopotam()
+    {
+        var w = new EcsWorld();
+        var s = new EcsSystems(w);
+        s.Add(new AllEntityComponentSwitcherSystem1());
+        s.Add(new AllEntityComponentSwitcherSystem2());
+
+        for (var i = 0; i < entities; i++)
+        {
+            var e = w.NewEntity();
+            var pool = w.GetPool<ComponentLeo1>();
+            ref ComponentLeo1 c1 = ref pool.Add(e);
+        }
+        s.Init();
+
+        for (var i = 0; i < iterations / 10; i++)
+        {
+            s.Run();
+        }
+
+        s.Destroy();
+    }
+
+    internal class AllEntityComponentSwitcherSystem1 : IEcsSystem, IEcsRunSystem
+    {
+        public void Run(IEcsSystems systems)
+        {
+            EcsWorld world = systems.GetWorld();
+            var filter1 = world.Filter<ComponentLeo1>().End();
+            var comps1 = world.GetPool<ComponentLeo1>();
+            var comps2 = world.GetPool<ComponentLeo2>();
+
+            foreach (var entity in filter1)
+            {
+                comps2.Add(entity);
+                comps1.Del(entity);
+            }
+        }
+    }
+
+    internal class AllEntityComponentSwitcherSystem2 : IEcsSystem, IEcsRunSystem
+    {
+        public void Run(IEcsSystems systems)
+        {
+            EcsWorld world = systems.GetWorld();
+            var filter2 = world.Filter<ComponentLeo2>().End();
+            var comps1 = world.GetPool<ComponentLeo1>();
+            var comps2 = world.GetPool<ComponentLeo2>();
+
+            foreach (var entity in filter2)
+            {
+                comps1.Add(entity);
+                comps2.Del(entity);
+            }
+        }
+    }
+
+    internal struct ComponentLeo1
+    {
+        public int Id;
+    }
+
+    internal struct ComponentLeo2
+    {
+    }
+
+}
---
+++ benchmark/benchmark.md
@@ -0,0 +1,5 @@
+|                      Method |     Mean |     Error |    StdDev |   Median |   Gen 0 | Allocated |
+|---------------------------- |---------:|----------:|----------:|---------:|--------:|----------:|
+| With debug checks           | 10.96 ms |  0.049 ms | 0.041 ms  |          | 31.2500 |    163 KB |
+| Filters enabled             | 9.690 ms | 0.1115 ms | 0.0988 ms |          | 31.2500 |    156 KB |
+| Default version             | 8.642 ms | 0.0926 ms | 0.0821 ms |          | 31.2500 |    156 KB |
